1149c9d5eceb11cd4b87bf9686dc31f9fd1b9b25
Christian Lopes Viana <christian.viana@gmail.com>
2019-08-19 20:55:04 -0300

    Rebase com Master

--- painelInterno/WebContent/javascript.js
+++ painelInterno/WebContent/javascript.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
 function limpaECarregaTabela(){
 	$.ajax({url: paginaDeDados , success: function(result){
-		$("#tabela").html("");
+		$("#interruptoresRow").html("");
 		insereLinhas(result);
 	}, cache: false});
 }
@@ -25,35 +25,33 @@ function limpaECarregaTabela(){
 function insereLinhas(result){
 	var pagina = JSON.parse(result);
 	var qtd = pagina.Dispo.length; 
+	
 	for(var numLinha=0; numLinha<qtd; numLinha++){
 		
-		var txtLinha  = '<tr>';
+			var txtLinha  = '<div class=".col-sm-">';
 
-			txtLinha += `<td id=tdLocal_${numLinha}>` + pagina.Dispo[numLinha].LOCAL + '</td>'; 			// Local
-			txtLinha += `<td id=tdModelo_${numLinha}>` + pagina.Dispo[numLinha].ESP_MOD + '</td>';			// Modelo
-			txtLinha += `<td id=tdEstado_${numLinha}>` + pagina.Dispo[numLinha].ESTADO + '</td>'; 			// Estado
-						
+			// Local
+			txtLinha += `<p id=pLocal_${numLinha}>` + pagina.Dispo[numLinha].LOCAL + '</p>'; 			
+									
+			
 			// LED
-			txtLinha += `<td id=tdBotao_${numLinha}>`;
-			txtLinha += '<label class=switch>';
-			txtLinha += `<input id=bt_${numLinha} `;
-			if(pagina.Dispo[numLinha].LED == '1') 
-				txtLinha += 'checked=true ';
+		  	txtLinha += `<label id=tdBotao_${numLinha} class="rocker rocker-small">`;
+		  	txtLinha += '<input type="checkbox"';
+		  	
+		  	if(pagina.Dispo[numLinha].LED == '1') 
+				txtLinha += ' checked=true ';
 	        txtLinha += `onclick="muda(this.id,${pagina.Dispo[numLinha].SEQ})"`; 
-	        txtLinha += 'type=checkbox>';
-			txtLinha += '<span class="slider"></span>';
-			txtLinha += '</label>';
-			txtLinha += '</td>';
-
-			txtLinha += `<td id=tdTemperatura_${numLinha}>` + pagina.Dispo[numLinha].ADC1 + '</td>'; 		// Temperatura 
-			txtLinha += `<td id=tdPressao_${numLinha}>` + pagina.Dispo[numLinha].ADC2 + '</td>'; 			// 	Pressão
-			txtLinha += `<td id=tdSequencial_${numLinha}>` + pagina.Dispo[numLinha].SEQ + '</td>'; 			// Sequencial
+		  			  	
+		  	txtLinha += '><span class="switch-left">I</span>';
+		  	txtLinha += '<span class="switch-right">O</span>';
+		  	txtLinha += '</label>';
 			
-			txtLinha += '</tr>';
+			txtLinha += '</div>';
 			
-		$("#tabela").append(txtLinha);
+		$("#interruptoresRow").append(txtLinha);
 	
   }
+	
 }
 
 // Usa AJAX pra só recarregar o botão que mudou, e recarrega a página novamente em alguns segundos
